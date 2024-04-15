a6449dc6597d12f8d6431d34e908b79a7cfeb343
Obskyur <parkourman.miller@gmail.com>
2024-04-14 20:00:17 -0700

    docs: Comment on JadenCase

--- Test JadenCasing/JadenCasing.cs
+++ Test JadenCasing/JadenCasing.cs
@@ -5,12 +5,11 @@ namespace Test_JadenCasing
 {
     internal static class JadenCase
     {
-        public static string ToJadenCase(this string phrase)
-        {
-            return String.Join(" ", phrase.Split().Select(word => Char.ToUpper(word[0]) + word[1..]));
+        public static string ToJadenCase(this string phrase) => 
+            // Using Linq:
+            String.Join(" ", phrase.Split().Select(word => Char.ToUpper(word[0]) + word[1..]));
 
             // Using CultureInfo:
             // return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
-        }
     }
 }
